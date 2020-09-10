398059e4b5b146ffbdf95b67a473414374eb8e46
courage173 <courageosemwengie@gmail.com>
2020-09-10 21:20:26 +0100

    created the update mutation and getting a single item query

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -6,6 +6,17 @@ const Mutations = {
             }
         }, info)
         return item
+    },
+    async updateItem(parent, args, ctx, info) {
+        const updates = { ...args }
+        delete updates.id
+        return await ctx.db.mutation.updateItem({
+            data: updates,
+            where: {
+                id: args.id
+            }
+        },
+            info)
     }
 };
 
