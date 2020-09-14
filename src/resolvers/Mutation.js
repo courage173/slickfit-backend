e4426b3bba3c8855e3a9f382c469adbaca97c649
courage173 <courageosemwengie@gmail.com>
2020-09-14 21:16:15 +0100

    created mutation for adding a user

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const Mutations = {
     async createItem(parent, args, ctx, info) {
         const item = await ctx.db.mutation.createItem({
@@ -24,6 +27,33 @@ const Mutations = {
         const item = await ctx.db.query.item({ where }, `{id title}`)
         //TODO- check for user permision
         return ctx.db.mutation.deleteItem({ where }, info)
+    },
+    async signup(parent, args, ctx, info) {
+
+        //lowercase email
+        args.email = args.email.toLowerCase()
+        //harsh the pasword
+        const password = await bcrypt.hash(args.password, 10)
+        //create users
+        const user = await ctx.db.mutation.createUser(
+            {
+                data: {
+                    ...args,
+                    password,
+                    permissions: { set: ['USER'] },
+                },
+            },
+            info
+        );
+
+        //create token
+        const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET)
+        //set cookies on the response
+        ctx.response.cookie('token', token, {
+            httpOnly: true,
+            maxAge: 1000 * 60 * 60 * 24 * 365
+        })
+        return user
     }
 };
 
