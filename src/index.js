b0a227871c9355cb3ce29dc22293272b83bbf6fc
courage173 <courageosemwengie@gmail.com>
2020-09-15 21:36:46 +0100

    updated the datamodel and created middleware to handle jwt and also get single user detail

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken');
 require('dotenv').config({ path: 'variables.env' })
 const createServer = require('./createServer')
 const db = require('./db')
@@ -5,6 +6,15 @@ const cookieParser = require('cookie-parser')
 
 const server = createServer()
 server.express.use(cookieParser())
+
+server.express.use((req, res, next) => {
+    const { token } = req.cookies
+    if (token) {
+        const { userId } = jwt.verify(token, process.env.APP_SECRET);
+        req.userId = userId
+    }
+    next()
+})
 server.start({
     cors: {
         credentials: true,
